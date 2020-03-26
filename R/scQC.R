cf0484d6ed36838da46e84ec939b78257ba99766
Daniel Osorio <dosorio@users.noreply.github.com>
2020-03-26 13:42:45 -0500

    Update scQC.R

--- R/scQC.R
+++ R/scQC.R
@@ -1,22 +1,24 @@
 #' @importFrom Matrix Matrix colSums
 #' @importFrom stats lm predict
-scQC <- function(X, mtThreshold = 0.1){
+scQC <- function(X, mtThreshold = 0.1, minLSize = 1000){
   if(class(X) == 'Seurat'){
     countMatrix <- X@assays$RNA@counts
   } else {
     countMatrix <- X
   }
   librarySize <- colSums(countMatrix)
+  countMatrix <- countMatrix[,librarySize >= minLSize]
+  librarySize <- colSums(countMatrix)
   mtCounts <- colSums(countMatrix[grep('^MT-',toupper(rownames(countMatrix))),])
   nGenes <- colSums(countMatrix != 0)
   mtProportion <- mtCounts/librarySize
-
+  
   genesLM <- lm(nGenes~librarySize)
   mtLM <- lm(mtCounts~librarySize)
-
+  
   genesLM <- as.data.frame(predict(genesLM, data.frame(librarySize), interval = 'prediction'))
   mtLM <- as.data.frame(predict(mtLM, data.frame(librarySize), interval = 'prediction'))
-
+  
   selectedCells <- mtCounts > mtLM$lwr & mtCounts < mtLM$upr & nGenes > genesLM$lwr & nGenes < genesLM$upr & mtProportion <= mtThreshold & librarySize < 2 * mean(librarySize)
   selectedCells <- colnames(countMatrix)[selectedCells]
   if(class(X) == 'Seurat'){
