c0ae786e5c12387e8d92967abc0b8b2819c48326
Daniel Osorio <dcosorioh@tamu.edu>
2020-04-12 13:52:52 -0500

    Data added

---
+++ inst/manuscript/TREM2/Code/TREM2_DataPrepatation.R
@@ -0,0 +1,32 @@
+library(Matrix)
+
+# Reading input files
+cellInfo <- read.csv('../Data/GSE130626_cell_info.csv.gz', stringsAsFactors = FALSE)
+geneInfo <- read.csv('../Data/GSE130626_gene_info.csv.gz', stringsAsFactors = FALSE)
+UMI <- read.csv('../Data/GSE130626_umi_counts.csv.gz', row.names = 1)
+
+# Replacing Ensembl to Symbols
+UMI <- Matrix(as.matrix(UMI))
+UMI <- UMI[geneInfo$gene_id,]
+rownames(UMI) <- make.unique(geneInfo$name)
+
+# Selecting treatment
+cellInfo <- cellInfo[cellInfo$treatment == 'cuprizone',]
+
+# Selecting cells
+WT <- cellInfo$cell_id[cellInfo$trem2_genotype == 'WT']
+KO <- cellInfo$cell_id[cellInfo$trem2_genotype == 'KO']
+
+# Filtering cells
+WT <- UMI[,WT]
+KO <- UMI[,KO]
+
+# Writing objects
+writeMM(WT, file = '../Data/matrix_WT.mtx')
+writeMM(KO, file = '../Data/matrix_KO.mtx')
+
+writeLines(rownames(WT), '../Data/geneNames_WT.txt')
+writeLines(rownames(KO), '../Data/geneNames_KO.txt')
+
+writeLines(colnames(WT), '../Data/barcodes_WT.txt')
+writeLines(colnames(KO), '../Data/barcodes_KO.txt')
