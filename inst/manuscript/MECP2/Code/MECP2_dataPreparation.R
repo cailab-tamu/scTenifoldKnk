3d021a5876d0487ca985b85e8e63f26e12aa3801
Daniel Osorio <dcosorioh@tamu.edu>
2020-04-15 21:09:39 -0500

    Update MECP2_dataPreparation.R

--- inst/manuscript/MECP2/Code/MECP2_dataPreparation.R
+++ inst/manuscript/MECP2/Code/MECP2_dataPreparation.R
@@ -17,8 +17,6 @@ writeMM(sm, file = '../Data/matrix_SRS3059998.mtx')
 writeLines(colnames(sm), '../Data/barcodes_SRS3059998.txt')
 writeLines(rownames(sm), '../Data/geneNames_SRS3059998.txt')
 
-
-
 cellID <- read.csv('../Data/SRA667466_SRS3059999.clusters.txt', header = FALSE, sep = ' ')
 download_html('https://panglaodb.se/list_clusters_and_cell_types.html?sra=SRA667466&srs=SRS3059999', file = '../Data/SRA667466_SRS3059999.clusters')
 clusterID <- readHTMLTable('../Data/SRA667466_SRS3059999.clusters', as.data.frame = TRUE)
