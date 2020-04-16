f2a106d9fb50d5d0992757e378c017509840a0eb
Daniel Osorio <dcosorioh@tamu.edu>
2020-04-15 20:34:19 -0500

    Update CFTR_dataProcessing.R

--- inst/manuscript/CFTR/Code/CFTR_dataProcessing.R
+++ inst/manuscript/CFTR/Code/CFTR_dataProcessing.R
@@ -31,7 +31,7 @@ colnames(SRS3161261) <- readLines('CFTR/Data/barcodes_SRS3161261.txt')
 SRS3161261 <- scQC(SRS3161261)
 SRS3161261 <- SRS3161261[rowMeans(SRS3161261 != 0) > 0.05,]
 SRS3161261 <- SRS3161261[!grepl('^Rp[[:digit:]]+|^Rpl|^Rps|^Mt-', rownames(SRS3161261), ignore.case = TRUE),]
-SRS3161261 <- scTenifoldKnk(SRS3161261)
+SRS3161261 <- scTenifoldKnk(SRS3161261, gKO = 'Cftr')
 save(SRS3161261, file = 'CFTR_SRS3161261.RData')
 
 SRS4245406 <- readMM('CFTR/Data/matrix_SRS4245406.mtx')
@@ -40,5 +40,5 @@ colnames(SRS4245406) <- readLines('CFTR/Data/barcodes_SRS4245406.txt')
 SRS4245406 <- scQC(SRS4245406)
 SRS4245406 <- SRS4245406[rowMeans(SRS4245406 != 0) > 0.05,]
 SRS4245406 <- SRS4245406[!grepl('^Rp[[:digit:]]+|^Rpl|^Rps|^Mt-', rownames(SRS4245406), ignore.case = TRUE),]
-SRS4245406 <- scTenifoldKnk(SRS4245406)
+SRS4245406 <- scTenifoldKnk(SRS4245406, gKO = 'Cftr')
 save(SRS4245406, file = 'CFTR_SRS4245406.RData')
