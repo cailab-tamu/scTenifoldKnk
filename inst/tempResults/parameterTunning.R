8bf745efe477d88595eb48dd7a602d26e40b0c13
Daniel Osorio <dcosorioh@tamu.edu>
2020-04-21 15:38:36 -0500

    Update parameterTunning.R

--- inst/tempResults/parameterTunning.R
+++ inst/tempResults/parameterTunning.R
@@ -2,7 +2,6 @@ library(UpSetR)
 library(pbapply)
 library(enrichR)
 
-
 folder <- 'all'
 q <- 0.9
 
@@ -40,36 +39,37 @@ gList_MECP2 <- readLines('gSet_Rett.txt')
 gList_HNF4AG <- c('SPDEF', 'ATOH1','MUC2','TFF3')
 
 allDatasets <- list(CFTR_1, CFTR_2, HNF4AG, HNF4ASMAD4, MECP2_1, MECP2_2, NKX21, TREM2, DMD)
-nGenes <- pbsapply(seq_len(10)[-1], function(Z){
-  dGenes <- lapply(allDatasets, function(X){
-    X$diffRegulation <- scTenifoldNet::dRegulation(X$manifoldAlignment[,seq_len(Z)])
-    X$diffRegulation <- X$diffRegulation[!grepl('^Rp[[:digit:]]+|^Rpl|^Rps|^mt-', X$diffRegulation$gene, ignore.case = TRUE),]
-    if('Hnf4a' %in% X$diffRegulation$gene[1:2]){
-      D <- X$diffRegulation[-c(1,2),]$distance
-      PV <- c(0,0,pchisq((D^2/mean(D^2)), df = 1, lower.tail = FALSE))
-    } else{
-      D <- X$diffRegulation[-1,]$distance
-      PV <- c(0,pchisq((D^2/mean(D^2)), df = 1, lower.tail = FALSE))
-    }
-    PA <- p.adjust(PV, method = 'fdr')
-    dGenes <- X$diffRegulation$gene[PA < 0.05]
-  })
-  M1 <- mean(toupper(dGenes[[1]]) %in% gList_CF)
-  M2 <- mean(toupper(dGenes[[2]]) %in% gList_CF)
-  M3 <- mean(toupper(dGenes[[3]]) %in% gList_HNF4AG)
-  M5 <- mean(toupper(dGenes[[5]]) %in% gList_MECP2)
-  M6 <- mean(toupper(dGenes[[6]]) %in% gList_MECP2)
-  M9 <- mean(toupper(dGenes[[9]]) %in% gList_DMD)
-  M10 <- mean(rowSums(fromList(dGenes[1:2])) == 2)
-  M11 <- mean(rowSums(fromList(dGenes[3:4])) == 2)
-  M12 <- mean(rowSums(fromList(dGenes[5:6])) == 2)
-  c(M1,M2,M3,M5,M6,M9,M10,M11,M12)
-})
-Z <- which.max(rowMeans(apply(nGenes,1,rank))) + 1
-Z
+
+# nGenes <- pbsapply(seq_len(10)[-1], function(Z){
+#   dGenes <- lapply(allDatasets, function(X){
+#     X$diffRegulation <- scTenifoldNet::dRegulation(X$manifoldAlignment[,seq_len(Z)])
+#     X$diffRegulation <- X$diffRegulation[!grepl('^Rp[[:digit:]]+|^Rpl|^Rps|^mt-', X$diffRegulation$gene, ignore.case = TRUE),]
+#     if('Hnf4a' %in% X$diffRegulation$gene[1:2]){
+#       D <- X$diffRegulation[-c(1,2),]$distance
+#       PV <- c(0,0,pchisq((D^2/mean(D^2)), df = 1, lower.tail = FALSE))
+#     } else{
+#       D <- X$diffRegulation[-1,]$distance
+#       PV <- c(0,pchisq((D^2/mean(D^2)), df = 1, lower.tail = FALSE))
+#     }
+#     PA <- p.adjust(PV, method = 'fdr')
+#     dGenes <- X$diffRegulation$gene[PA < 0.05]
+#   })
+#   M1 <- mean(toupper(dGenes[[1]]) %in% gList_CF)
+#   M2 <- mean(toupper(dGenes[[2]]) %in% gList_CF)
+#   M3 <- mean(toupper(dGenes[[3]]) %in% gList_HNF4AG)
+#   M5 <- mean(toupper(dGenes[[5]]) %in% gList_MECP2)
+#   M6 <- mean(toupper(dGenes[[6]]) %in% gList_MECP2)
+#   M9 <- mean(toupper(dGenes[[9]]) %in% gList_DMD)
+#   M10 <- mean(rowSums(fromList(dGenes[1:2])) == 2)
+#   M11 <- mean(rowSums(fromList(dGenes[3:4])) == 2)
+#   M12 <- mean(rowSums(fromList(dGenes[5:6])) == 2)
+#   c(M1,M2,M3,M5,M6,M9,M10,M11,M12)
+# })
+# Z <- which.max(rowMeans(apply(nGenes,1,rank))) + 1
+# Z
 
 dGenes <- lapply(allDatasets, function(X){
-  X$diffRegulation <- scTenifoldNet::dRegulation(X$manifoldAlignment[,seq_len(4)])
+  X$diffRegulation <- scTenifoldNet::dRegulation(X$manifoldAlignment[,seq_len(2)])
   X$diffRegulation <- X$diffRegulation[!grepl('^Rp[[:digit:]]+|^Rpl|^Rps|^mt-', X$diffRegulation$gene, ignore.case = TRUE),]
   if('Hnf4a' %in% X$diffRegulation$gene[1:2]){
     D <- X$diffRegulation[-c(1,2),]$distance
