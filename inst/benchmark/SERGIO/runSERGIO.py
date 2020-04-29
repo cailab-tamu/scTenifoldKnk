294007f59144f06819793b072c06e1d93a2950d6
Daniel Osorio <dcosorioh@tamu.edu>
2020-04-29 15:46:13 -0500

    SERGIO simulation added

--- inst/benchmark/SERGIO/runSERGIO.py
+++ inst/benchmark/SERGIO/runSERGIO.py
@@ -1,10 +1,10 @@
 import numpy as np
 from sergio import sergio
 np.random.seed(1)
-sim = sergio(number_genes=10, number_bins = 1, number_sc = 3000, 
-             noise_params = 1, decays=18, sampling_state=50,
+sim = sergio(number_genes=100, number_bins = 1, number_sc = 3000, 
+             noise_params = 1, decays=20, sampling_state=1,
              noise_type='dpd')
-sim.build_graph('targetFile.py', 'regFile.py', shared_coop_state=2)
+sim.build_graph('targetFile.py', 'regFile.py', shared_coop_state=10)
 np.random.seed(1)
 sim.simulate()
 np.random.seed(1)
