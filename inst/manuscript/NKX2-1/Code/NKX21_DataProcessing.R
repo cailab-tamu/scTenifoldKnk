71951b0ca2d7feec2240f0827d3abf8679f75717
Daniel Osorio <dcosorioh@tamu.edu>
2020-10-12 09:08:13 -0500

    NKX21 DR added

--- inst/manuscript/NKX2-1/Code/NKX21_DataProcessing.R
+++ inst/manuscript/NKX2-1/Code/NKX21_DataProcessing.R
@@ -42,6 +42,7 @@ source('https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/pl
 source('https://raw.githubusercontent.com/dosorio/utilities/master/idConvert/hsa2mmu_SYMBOL.R')
 
 load('../Results/GSM3716703.RData')
+write.csv(GSM3716703$diffRegulation, '../Results/dr_GSM3716703.csv')
 #GSM3716703 <- WT
 markerGenes <- read.csv('../Data/pnas.1906663116.sd01.csv', stringsAsFactors = FALSE, row.names = 1)
 markerGenes$T.test.p.value <- as.numeric(markerGenes$T.test.p.value)
