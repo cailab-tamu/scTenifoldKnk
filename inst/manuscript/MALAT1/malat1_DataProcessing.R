afffdaf894ca6cdf04cbb19029d93725149b3bde
Daniel Osorio <dcosorioh@tamu.edu>
2020-10-14 11:28:27 -0500

    MALAT1 Update

--- inst/manuscript/MALAT1/malat1_DataProcessing.R
+++ inst/manuscript/MALAT1/malat1_DataProcessing.R
@@ -10,11 +10,11 @@ library(ggrepel)
 library(ggplot2)
 source('https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/plotKO.R')
 
-mmuKEGG <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=KEGG_2019_Mouse')
-BIOP <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=BioPlanet_2019')
-REACTOME <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=Reactome_2016')
-goBP <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=GO_Biological_Process_2018')
-WP <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=WikiPathways_2019_Human')
+mmuKEGG <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=KEGG_2019_Mouse')
+BIOP <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=BioPlanet_2019')
+REACTOME <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=Reactome_2016')
+goBP <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=GO_Biological_Process_2018')
+WP <- gmtPathways('https://maayanlab.cloud/Enrichr/geneSetLibrary?mode=text&libraryName=WikiPathways_2019_Human')
 
 #### DE ####
 WT <- readMM('WT.mtx')
@@ -46,6 +46,7 @@ dev.off()
 #### Volcano ####
 load('betaMALATko.RData')
 DE <- FindMarkers(ALL, ident.1 = 'KO', ident.2 = 'WT', test.use = 'MAST', logfc.threshold = 0)
+drGenes <- MALAT1$diffRegulation$gene[MALAT1$diffRegulation$p.adj < 0.05]
 
 library(ggrepel)
 DF <- data.frame(FC = DE$avg_logFC, P = -log10(DE$p_val_adj), G = rownames(DE))
@@ -59,15 +60,12 @@ png('volcanoMalat1.png', width = 1200, height = 1200, res = 300)
 ggplot(DF, aes(FC,P, label = G)) + 
   geom_point(col = gCol) + 
   xlim(c(-1,1)) + 
-  geom_text_repel(size = 3, segment.alpha = 0.2, force = 10, segment.size = 0.1) +
+  geom_text_repel(size = 3, segment.alpha = 0.2, force = 10, segment.size = 0.1, aes(fontface = ifelse(DF$G %in% drGenes, 2, 1))) +
   theme_bw() + 
   xlab(expression(log[2]~(Fold-Change))) +
   ylab(expression(-log[10]~(P-value)))
 dev.off()
 
-
-FC <- DE$avg_logFC #* -log10(DE$p_val_adj)
-names(FC) <- toupper(rownames(DE))
 
 #### DR ####
 load('betaMALATko.RData')
@@ -79,14 +77,13 @@ png('egoMalat1.png', width = 6500,height = 5000, res = 300, pointsize = 20, bg =
 plotKO(MALAT1, 'Malat1')
 dev.off()
 
-
+#### Comparison ####
 DR <- MALAT1$diffRegulation
 Z <- DR$Z
 names(Z) <- toupper(DR$gene)
 writeLines(DR$gene[DR$p.adj < 0.05])
-
-
-#### Comparison ####
+FC <- DE$avg_logFC #* -log10(DE$p_val_adj)
+names(FC) <- toupper(rownames(DE))
 sGenes <- intersect(names(Z), names(FC))
 Z <- Z[sGenes]
 FC <- abs(FC[sGenes])
@@ -102,9 +99,13 @@ eDE <- fgseaMultilevel(mmuKEGG, FC)
 eDR <- eDR[eDR$NES > 0 & eDR$padj < 0.05,]
 eDE <- eDE[eDE$NES > 0 & eDE$padj < 0.05,]
 
-png('upset.png', width = 750, height = 1500, res = 300)
-upset(fromList(list(DE=eDE$pathway, DR=eDR$pathway)))
+png('upset.png', width = 900, height = 900, res = 300, pointsize = 1.2)
+upset(fromList(list(DE=eDE$pathway, DR=eDR$pathway)),mb.ratio = c(0.8, 0.2))
 dev.off()
+
+
+phyper(m = 85, n = 303-85, q = 8, k = 11, lower.tail = FALSE)
+
 
 lZ <- names(sort(Z, decreasing = TRUE))
 lFC <- names(sort(FC, decreasing = TRUE))
@@ -131,27 +132,45 @@ kD <- fgsea(REACTOME, FC, nperm = 1e6)
 set.seed(1)
 kE <- fgsea(goBP, FC, nperm = 1e6)
 k <- rbind(kA,kB,kC,kD,kE)
-k <- k[k$padj < 0.05,]
+k1 <- k[k$padj < 0.05,]
+k1$leadingEdge <- unlist(lapply(k1$leadingEdge, function(X){paste0(X, collapse = ';')}))
+write.csv(k1, 'deEnrichment.csv')
 
+set.seed(1)
+kA <- fgsea(mmuKEGG, Z, nperm = 1e6)
+set.seed(1)
+kB <- fgsea(BIOP, Z, nperm = 1e6)
+set.seed(1)
+kC <- fgsea(WP, Z, nperm = 1e6)
+set.seed(1)
+kD <- fgsea(REACTOME, Z, nperm = 1e6)
+set.seed(1)
+kE <- fgsea(goBP, Z, nperm = 1e6)
+k <- rbind(kA,kB,kC,kD,kE)
+k2 <- k[k$padj < 0.05,]
+k2$leadingEdge <- unlist(lapply(k2$leadingEdge, function(X){paste0(X, collapse = ';')}))
+write.csv(k2, 'drEnrichment.csv')
+
+k <- k2
 png('gsea1_Malat1.png', width = 1000, height = 1000, res = 300)
 NES <- round(k[k$pathway %in% 'Insulin secretion',]$NES,2)
 FDR <- formatC(k[k$pathway %in% 'Insulin secretion',]$padj, format = 'e', digits = 2)
 plotEnrichment(mmuKEGG$`Insulin secretion`, FC) + 
   xlab('Gene rank') + 
   ylab('Enrichment Score') + 
-  theme(plot.title = element_text(face = 2)) +
-  labs(title = 'Insulin secretion', subtitle = paste0('NES= ', NES, ', FDR = ', FDR))
+  theme(plot.title = element_text(face = 2, size = 25)) +
+  labs(title = 'Insulin\nsecretion', subtitle = paste0('FDR = ', FDR))
 dev.off()
 
-png('gsea2_Malat1.png', width = 1000, height = 1000, res = 300)
-NES <- round(k[k$pathway %in% 'cellular respiration (GO:0045333)',]$NES,2)
-FDR <- formatC(k[k$pathway %in% 'cellular respiration (GO:0045333)',]$padj, format = 'e', digits = 2)
-plotEnrichment(goBP$`cellular respiration (GO:0045333)`, FC) + 
-  xlab('Gene rank') + 
-  ylab('Enrichment Score') +
-  theme(plot.title = element_text(face = 2)) +
-  labs(title = 'Cellular Respiration GO:0045333', subtitle = paste0('NES= ', NES, ', FDR = ', FDR))
-dev.off()
+# png('gsea2_Malat1.png', width = 1000, height = 1000, res = 300)
+# NES <- round(k[k$pathway %in% 'cellular respiration (GO:0045333)',]$NES,2)
+# FDR <- formatC(k[k$pathway %in% 'cellular respiration (GO:0045333)',]$padj, format = 'e', digits = 2)
+# plotEnrichment(goBP$`cellular respiration (GO:0045333)`, FC) + 
+#   xlab('Gene rank') + 
+#   ylab('Enrichment Score') +
+#   theme(plot.title = element_text(face = 2)) +
+#   labs(title = 'Cellular Respiration GO:0045333', subtitle = paste0('NES= ', NES, ', FDR = ', FDR))
+# dev.off()
 
 png('gsea3_Malat1.png', width = 1000, height = 1000, res = 300)
 NES <- round(k[k$pathway %in% 'Circadian rhythm related genes WP3594',]$NES,2)
@@ -159,6 +178,6 @@ FDR <- formatC(k[k$pathway %in% 'Circadian rhythm related genes WP3594',]$padj, 
 plotEnrichment(WP$`Circadian rhythm related genes WP3594`, FC) + 
   xlab('Gene rank') + 
   ylab('Enrichment Score') + 
-  theme(plot.title = element_text(face = 2)) +
-  labs(title = 'Circadian rhythm related genes', subtitle = paste0('NES= ', NES, ', FDR = ', FDR))
+  theme(plot.title = element_text(face = 2, size = 25)) +
+  labs(title = 'Circadian rhythm\nrelated genes', subtitle = paste0('FDR = ', FDR))
 dev.off()
