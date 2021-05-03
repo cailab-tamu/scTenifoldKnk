a899a6b1d9d5c3684ac738b6c0fe9875f10c8d5d
Daniel Osorio <dcosorioh@tamu.edu>
2021-05-03 07:03:34 -0500

    Update dRegulation.R

--- R/dRegulation.R
+++ R/dRegulation.R
@@ -1,25 +1,25 @@
 #' @importFrom stats dist p.adjust pchisq
 #' @importFrom MASS boxcox
 dRegulation <- function(manifoldOutput, gKO){
-  
+
   geneList <- rownames(manifoldOutput)
   geneList <- geneList[grepl('^X_', geneList)]
   geneList <- gsub('^X_','', geneList)
   nGenes <- length(geneList)
-  
+
   eGenes <- nrow(manifoldOutput)/2
-  
+
   eGeneList <- rownames(manifoldOutput)
   eGeneList <- eGeneList[grepl('^Y_', eGeneList)]
   eGeneList <- gsub('^Y_','', eGeneList)
-  
+
   if(nGenes != eGenes){
     stop('Number of identified and expected genes are not the same')
   }
   if(!all(eGeneList == geneList)){
     stop('Genes are not ordered as expected. X_ genes should be followed by Y_ genes in the same order')
   }
-  
+
   dMetric <- sapply(seq_len(nGenes), function(G){
     X <- manifoldOutput[G,]
     Y <- manifoldOutput[(G+nGenes),]
@@ -28,20 +28,24 @@ dRegulation <- function(manifoldOutput, gKO){
     O <- as.numeric(O)
     return(O)
   })
-  
+
   ### BOX-COX
   lambdaValues <- seq(-2,2,length.out = 1000)
   lambdaValues <- lambdaValues[lambdaValues != 0]
-  BC <- MASS::boxcox(dMetric~1, plot=FALSE, lambda = lambdaValues)
-  BC <- BC$x[which.max(BC$y)]
-  if(BC < 0){
-    nD <- 1/(dMetric ^ BC)
+  BC <- try(MASS::boxcox(dMetric~1, plot=FALSE, lambda = lambdaValues), silent = TRUE)
+  if(class(BC) == 'try-error'){
+    nD <- dMetric
   } else {
-    nD <- dMetric ^ BC
+    BC <- BC$x[which.max(BC$y)]
+    if(BC < 0){
+      nD <- 1/(dMetric ^ BC)
+    } else {
+      nD <- dMetric ^ BC
+    }
   }
   Z <- scale(nD)
   dOut <- data.frame(
-    gene = geneList, 
+    gene = geneList,
     distance = dMetric,
     Z = Z
     )
