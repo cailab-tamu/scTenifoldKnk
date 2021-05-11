022ce0d22e40138a7d994f5dead8f6066a62747f
James Cai <jamescai@genomezoo.net>
2021-05-11 14:09:46 -0500

    Update knk3_buildPerturbationLandscape.m

--- MATLAB/knk3_buildPerturbationLandscape.m
+++ MATLAB/knk3_buildPerturbationLandscape.m
@@ -3,7 +3,7 @@ function [F]=knk3_buildPerturbationLandscape(A,genelist)
 n=length(genelist);
 assert(n==size(A,1));
 F=zeros(n,n);
-    parfor k=1:n
+    for k=1:n
         k
         [t]=knk2_knockoutTargetGene(A,genelist,genelist(k),false);
         F(:,k)=t.drdist;
