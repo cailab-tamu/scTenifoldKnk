1ef751ba978a2d8b13aa52b6b19e6bfe6abe92af
James Cai <jamescai@genomezoo.net>
2022-01-14 16:59:44 -0600

    .

--- MATLAB/knk1_makeRefNetwork.m
+++
@@ -1,9 +0,0 @@
-function [A,outfile]=knk1_makeRefNetwork(X,genelist)
-A=sc_pcnetdenoised(X);
-if nargout>1
-    tstr=matlab.lang.makeValidName(datestr(datetime));
-    outfile=sprintf('A_%s',tstr);
-    outfile=fullfile(pwd,[outfile,'.mat']);
-    save(outfile,'A','genelist','-v7.3');
-end
-end
